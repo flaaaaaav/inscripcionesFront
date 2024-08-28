82ee26c423b9e1b5b589f673db12eae0353ec498
ferutizi <fer.utizi@gmail.com>
2024-08-28 17:33:56 -0300

    add: material ui, createTheme, paleta de colores

--- ui-inscripciones/src/main.tsx
+++ ui-inscripciones/src/main.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './auth';
 import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0b7077',
+      dark: '#34495e',
+      light: '#d2e6e4',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#fd661f',
+    },
+  },
+});
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
@@ -10,7 +26,9 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <AuthProvider>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </AuthProvider>
     </React.StrictMode>
   );
--- ui-inscripciones/src/pages/Home.tsx
+++ ui-inscripciones/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Auth } from '../auth';
+import Button from '@mui/material/Button'
+import { Container } from '@mui/material';
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
@@ -15,11 +17,32 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Inscripciones</h1>
+    <Container>
+      <nav className='bg-dark'>
+        {/* Logo */}
+        <Container sx={{backgroundColor: '#34495e'}} >
+          <a>Inicio</a>
+          <a>Cursos</a>
+          <a>Proyectos</a>
+        </Container>
+        <div>
+          <Button variant='contained' color='secondary' sx={{backgroundColor: '#fff', color: '#0b7077'}} >INICIAR SESION</Button>
+          <Button variant='contained'>REGISTRARSE</Button>
+        </div>
+      </nav>
+      <section className='bg-dark'>
+        <div>
+          <span>Nunca pares de aprender</span>
+          <h2>Desarrolla tus habilidades con cursos en línea</h2>
+          <Button variant='contained' color='secondary' sx={{color: '#fff'}}>VER CURSOS</Button>
+        </div>
+        {/* Imagen */}
+      </section>
+
+
       <Auth />
       <button onClick={handleProfileClick}>Perfil de usuario</button>
-    </div>
+    </Container>
   );
 };
 
