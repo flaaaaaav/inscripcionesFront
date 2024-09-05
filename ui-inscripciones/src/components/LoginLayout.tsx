e70caa368c82f64da52a036325f55f847a628acf
ferutizi <fer.utizi@gmail.com>
2024-09-05 19:20:07 -0300

    add: create layout for login pages

---
+++ ui-inscripciones/src/components/LoginLayout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+interface LoginLayoutProps {
+  children: ReactNode
+}
+
+export default function LoginLayout({children}: LoginLayoutProps) {
+  return(
+    <>
+      <Typography variant="h4" color="primary.dark" sx={{margin: "3rem", marginBottom: "0"}}>Punto&Aprende</Typography>
+      <Container sx={{ display: {xs: "block", md:"grid"}, width: "100dvw", height: "calc(100dvh - 8.625rem)", alignItems: "center",  placeContent: "center",  /* marginTop: "8.625rem", */ padding: {xs: "0 40px"}, gridTemplateColumns: "1fr 1fr", gap: "2rem", fontFamily: "Poppins, sans-serif"}}>
+        {children}
+      </Container>
+    </>
+  )
+}
